648cf2a68b8b183ae2e0efc173b06b1cafe98916
Serhii <ex@example.com>
2023-05-09 10:23:51 +0300

    fix algorithm change

--- index.php
+++ index.php
@@ -1,43 +1,42 @@
 <?php
-	$start = microtime(true);
-// Подключение к базе данных
-	$dsn = "mysql:host=localhost;dbname=twotest";
-	$username = "root";
-	$password = "";
-	try {
-		$pdo = new PDO($dsn, $username, $password);
-	} catch (PDOException $e) {
-		die("Ошибка подключения к базе данных: " . $e->getMessage());
-	}
-// Запрос на получение всех категорий
-	$sql = "SELECT * FROM categories";
-	$stmt = $pdo->prepare($sql);
-	$stmt->execute();
-	$categories = $stmt->fetchAll(PDO::FETCH_ASSOC);
-// Функция для построения дерева категорий
-	function buildCategoryTree($categories, $parent_id = 0)
-	{
-		$tree = array();
-		foreach ($categories as $category) {
-			if ($category['parent_id'] == $parent_id) {
-				$id = $category['categories_id'];
-				// Рекурсивный вызов функции для получения подкатегорий
-				$subcategories = buildCategoryTree($categories, $id);
-				// Добавление категории в дерево
-				if (empty($subcategories)) {
-					$tree[$id] = $id;
-				} else {
-					$tree[$id] = $subcategories;
-				}
-			}
-		}
-		return $tree;
-	}
-// Построение дерева категорий
-	$result = buildCategoryTree($categories);
-	echo '<pre>';
-	print_r($result);
-	echo '</pre>';
-	$end = microtime(true); // сохраняем текущее время в переменной $end
-	$time = $end - $start; // вычисляем разницу времени в секундах
-	echo "Время выполнения скрипта: $time секунд";
+    $start = microtime(true);
+
+    // Подключение к базе данных
+    $dsn = "mysql:host=localhost;dbname=twotest";
+    $username = "root";
+    $password = "";
+    
+    try {
+        $pdo = new PDO($dsn, $username, $password);
+    } catch (PDOException $e) {
+        die("Ошибка подключения к базе данных: " . $e->getMessage());
+    }
+    
+    // Запрос на получение всех категорий и их идентификаторов
+    $sql = "SELECT categories_id, categories_name, parent_id FROM categories ORDER BY parent_id ASC";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+    $categories = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+    // Построение дерева категорий
+    $tree = array();
+    foreach ($categories as $category) {
+        $id = $category['categories_id'];
+        $parent = $category['parent_id'];
+        $name = $category['categories_name'];
+        $node = array('name' => $name);
+        if ($parent == 0) {
+            $tree[$id] = $node;
+        } else {
+            $tree[$parent]['children'][$id] = $node;
+        }
+    }
+    
+    echo '<pre>';
+    print_r($tree);
+    echo '</pre>';
+    
+    $end = microtime(true);
+    $time = $end - $start;
+    echo "Время выполнения скрипта: $time секунд";
+    
